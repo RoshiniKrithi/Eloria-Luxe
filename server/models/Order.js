43c4dffd9fd33826e38f702bb24bff25b4b601c5
Roshini Krithi <roshinikrithi0@gmail.com>
2026-03-13 14:58:04 +0530

    feat(order): add order schema for handling customer purchases

---
+++ server/models/Order.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const orderSchema = mongoose.Schema({
+    user: {
+        type: String, // Simplification: using string for email or ID
+        required: true
+    },
+    orderItems: [
+        {
+            name: { type: String, required: true },
+            qty: { type: Number, required: true },
+            image: { type: String, required: true },
+            price: { type: Number, required: true },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: 'Product',
+            },
+        },
+    ],
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0,
+    },
+    isPaid: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
+    paidAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    timestamps: true
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
